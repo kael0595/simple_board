bd73027d65c62d363de0e91b187fb22dcee5dca8
kael0595 <changgyu549@gmail.com>
2025-03-11 19:16:57 +0900

    BoardVO 작성

---
+++ src/main/java/com/practice/simple_board/board/vo/BoardVO.java
@@ -0,0 +1,22 @@
+package com.practice.simple_board.board.vo;
+
+import com.practice.simple_board.member.vo.MemberVO;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BoardVO {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private MemberVO author;
+
+    private LocalDateTime createDt;
+
+    private LocalDateTime updateDt;
+}
