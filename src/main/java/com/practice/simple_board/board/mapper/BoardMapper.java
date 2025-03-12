82e8d5fadb21c46ca727bd7079b5f6e568824042
kael0595 <changgyu549@gmail.com>
2025-03-12 17:33:30 +0900

    게시글 목록 생성

--- src/main/java/com/practice/simple_board/board/mapper/BoardMapper.java
+++ src/main/java/com/practice/simple_board/board/mapper/BoardMapper.java
@@ -3,8 +3,13 @@ package com.practice.simple_board.board.mapper;
 import com.practice.simple_board.board.vo.BoardVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BoardMapper {
 
     void create(BoardVO boardVO);
+
+    List<BoardVO> selectAll();
+
 }
