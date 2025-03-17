e732834d487e34c4c8df54b29e134edb848a570b
kael0595 <changgyu549@gmail.com>
2025-03-17 19:50:32 +0900

    기본적인 spring security 설정 생성

--- src/main/java/com/practice/simple_board/base/configuration/SecurityConfig.java
+++ src/main/java/com/practice/simple_board/base/configuration/SecurityConfig.java
@@ -1,14 +1,37 @@
 package com.practice.simple_board.base.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
-@Component
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .headers((headers) ->
+                        headers.addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                        )))
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers("/", "/member/join", "/member/login", "/board/list",
+                                        "/board/detail/**", "/member/logout").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
     }
 }
