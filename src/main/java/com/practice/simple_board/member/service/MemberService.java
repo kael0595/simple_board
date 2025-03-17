2385cef065ea851715808cde46604c267099e979
kael0595 <changgyu549@gmail.com>
2025-03-17 19:39:19 +0900

    회원가입에 password encoder 적용

--- src/main/java/com/practice/simple_board/member/service/MemberService.java
+++ src/main/java/com/practice/simple_board/member/service/MemberService.java
@@ -2,17 +2,22 @@ package com.practice.simple_board.member.service;
 
 import com.practice.simple_board.member.mapper.MemberMapper;
 import com.practice.simple_board.member.vo.MemberVO;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final PasswordEncoder passwordEncoder;
+
     private final MemberMapper memberMapper;
 
     public void join(MemberVO memberVO) {
+
+        memberVO.setPassword(passwordEncoder.encode(memberVO.getPassword()));
+
         memberMapper.createMember(memberVO);
     }
 
