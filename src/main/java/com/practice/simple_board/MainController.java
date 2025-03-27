87367fb8216816c72bbb421b212c9ed29cf885df
kael0595 <changgyu549@gmail.com>
2025-03-27 17:15:36 +0900

    HttpSession을 통해 로그인 정보 찾는 방법 -> Principal을 통해 로그인 찾아오는 방법으로 수정

--- src/main/java/com/practice/simple_board/MainController.java
+++ src/main/java/com/practice/simple_board/MainController.java
@@ -2,11 +2,12 @@ package com.practice.simple_board;
 
 import com.practice.simple_board.member.service.MemberService;
 import com.practice.simple_board.member.vo.MemberVO;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,11 +17,11 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model,
-                        HttpSession session) {
+                        Principal principal) {
 
-        if (session.getAttribute("member") != null) {
+        if (principal.getName() != null) {
 
-            MemberVO member = (MemberVO) session.getAttribute("member");
+            MemberVO member = memberService.selectMemberByMemberId(principal.getName());
 
             model.addAttribute("member", member);
         }
