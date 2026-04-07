7131a374819bdca49fa3fced5e9f6570c8b3e77b
kael0595 <changgyu549@gmail.com>
2026-04-07 21:16:36 +0900

    회원가입 작성

--- src/main/java/com/example/demo/member/contoller/MemberController.java
+++ src/main/java/com/example/demo/member/contoller/MemberController.java
@@ -2,9 +2,12 @@ package com.example.demo.member.contoller;
 
 import com.example.demo.member.dto.MemberDto;
 import com.example.demo.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +17,21 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping("/signup")
+    public String signup(MemberDto memberDto) {
+        return "members/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid MemberDto memberDto,
+                         BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "members/signup";
+        }
+
+        memberService.signup(memberDto);
+
+        return "redirect:/";
+    }
 }
--- src/main/java/com/example/demo/member/service/MemberService.java
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -1,12 +1,25 @@
 package com.example.demo.member.service;
 
+import com.example.demo.member.dto.MemberDto;
+import com.example.demo.member.entity.Member;
 import com.example.demo.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public void signup(MemberDto memberDto) {
+
+        Member member = new Member();
+        member.setEmail(memberDto.getEmail());
+        member.setPassword(memberDto.getPassword());
+        member.setCreatedAt(LocalDateTime.now());
+        memberRepository.save(member);
+    }
 }
