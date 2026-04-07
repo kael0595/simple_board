6ac8820c9254e0bcc58645a3e025de51d9e5c580
kael0595 <changgyu549@gmail.com>
2026-04-07 22:12:59 +0900

    spring security의 로그인/로그아웃을 사용하기 위한 userdetailsservice 작성

---
+++ src/main/java/com/example/demo/member/service/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.example.demo.member.service;
+
+import com.example.demo.member.entity.Member;
+import com.example.demo.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<Member> _member = memberRepository.findByEmail(email);
+
+        if (_member.isEmpty()) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+
+        Member member = _member.get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if (member.getEmail().startsWith("admin")) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        } else {
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        return new User(member.getEmail(), member.getPassword(), authorities);
+    }
+}
