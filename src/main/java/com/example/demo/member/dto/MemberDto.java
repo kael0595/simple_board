dd9aa796d6d5469633086ed266d2f6764ecb2947
kael0595 <changgyu549@gmail.com>
2026-04-07 21:03:36 +0900

    memberDto 작성

---
+++ src/main/java/com/example/demo/member/dto/MemberDto.java
@@ -0,0 +1,23 @@
+package com.example.demo.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String passwordCnf;
+
+    private String name;
+
+    private String phone;
+}
