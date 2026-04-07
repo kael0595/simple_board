786b8b8ecf1334eedefb029576770d42095d5afb
kael0595 <changgyu549@gmail.com>
2026-04-07 22:07:29 +0900

    회원가입시 password 인코딩 추가

--- src/main/java/com/example/demo/member/service/MemberService.java
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.example.demo.member.dto.MemberDto;
 import com.example.demo.member.entity.Member;
 import com.example.demo.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,11 +15,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     public void signup(MemberDto memberDto) {
 
         Member member = new Member();
         member.setEmail(memberDto.getEmail());
-        member.setPassword(memberDto.getPassword());
+        member.setPassword(passwordEncoder.encode(memberDto.getPassword()));
         member.setCreatedAt(LocalDateTime.now());
         memberRepository.save(member);
     }
