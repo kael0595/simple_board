2016ac133e88e6216c4462afb8e7a4bddcf59dc6
kael0595 <changgyu549@gmail.com>
2026-04-07 21:14:07 +0900

    member controller, service, repository 작성

---
+++ src/main/java/com/example/demo/member/contoller/MemberController.java
@@ -0,0 +1,17 @@
+package com.example.demo.member.contoller;
+
+import com.example.demo.member.dto.MemberDto;
+import com.example.demo.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+}
