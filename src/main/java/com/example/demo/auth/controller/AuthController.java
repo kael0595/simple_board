dd0e133039c9641fb36db46f3ea1360382ed5676
kael0595 <changgyu549@gmail.com>
2026-04-08 22:24:33 +0900

    인증메일 전송 및 인증 메서드 작성

--- src/main/java/com/example/demo/auth/controller/AuthController.java
+++ src/main/java/com/example/demo/auth/controller/AuthController.java
@@ -1,18 +1,38 @@
 package com.example.demo.auth.controller;
 
+import com.example.demo.auth.service.AuthService;
 import com.example.demo.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
     @GetMapping("/login")
     public String login(MemberDto memberDto) {
         return "members/login";
+    }
+
+    @PostMapping("/email/send")
+    public ResponseEntity<?> sendEmail(@RequestParam String email) {
+
+        authService.sendVerificationCode(email);
+
+        return ResponseEntity.ok("인증 메일 발송 완료");
+    }
+
+    @PostMapping("/email/verify")
+    public ResponseEntity<?> verifyEmail(@RequestParam String email,
+                                         @RequestParam String code) {
+
+        authService.verifyCode(email, code);
+
+        return ResponseEntity.ok("인증 성공");
     }
 }
