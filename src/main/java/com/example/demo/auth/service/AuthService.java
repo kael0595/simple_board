cfff42005cfbe3bcaeaae838f741b2f3ffe40a6b
kael0595 <changgyu549@gmail.com>
2026-04-15 01:06:44 +0900

    인증상태 DB 저장을 위한 메서드 수정

--- src/main/java/com/example/demo/auth/service/AuthService.java
+++ src/main/java/com/example/demo/auth/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.example.demo.auth.service;
 
+import com.example.demo.auth.entity.EmailVerification;
 import com.example.demo.auth.repository.VerificationRepository;
 import com.example.demo.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +20,15 @@ public class AuthService {
 
         String code = generateCode();
 
-        verificationRepository.save(email, code);
+        EmailVerification emailVerification = verificationRepository
+                .findByEmail(email).orElse(new EmailVerification());
+
+        emailVerification.setCode(code);
+        emailVerification.setEmail(email);
+        emailVerification.setExpireTime(LocalDateTime.now().plusMinutes(5));
+        emailVerification.setVerified(false);
+
+        verificationRepository.save(emailVerification);
 
         emailService.sendEmail(email, code);
 
@@ -28,19 +39,22 @@ public class AuthService {
         return String.valueOf((int) ((Math.random() * 900000) + 100000));
     }
 
-    public void verifyCode(String email, String code) {
+    public void verify(String email, String code) {
 
-        String saveCode = verificationRepository.findByEmail(email);
+        EmailVerification emailVerification = verificationRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("인증 요청이 없습니다."));
 
-        if(saveCode == null) {
-            throw new RuntimeException("인증 요청 없음");
+        if (!emailVerification.getCode().equals(code)) {
+            throw new RuntimeException("인증번호가 일치하지 않습니다.");
         }
 
-        if (!saveCode.equals(code)) {
-            throw new RuntimeException("인증번호 불일치");
+        if (emailVerification.getExpireTime().isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("인증 시간이 만료되었습니다.");
         }
 
-        verificationRepository.remove(email);
+        emailVerification.verify();
+
+        emailVerification.setCode(null);
 
     }
 }
